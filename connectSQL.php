3d67ed1bfe1427113f7e0292eb0380460b6f6785
dung1202 <phamtiendung12022001@gmail.com>
2023-09-01 15:59:27 +0700

    update

---
+++ connectSQL.php
@@ -0,0 +1,9 @@
+<?php
+  $servername = "localhost";
+  $username = "tts_ptdung";
+  $password = "tts_ptdung";
+  $database = "tts_ptdung";
+  $conn = new mysqli($servername, $username, $password, $database);
+  // Tai khoan dang nhap: student
+  // Mat khau dang nhap: 123456a@A
+?>
